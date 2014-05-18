ae2090a95391444886450c0af635545e4e6e357a
themattharris <mharris@twitter.com>
2014-05-18 10:59:27 -0700

    dev version header

--- tmhOAuth.php
+++ tmhOAuth.php
@@ -7,14 +7,14 @@
  * REST requests. OAuth authentication is sent using an Authorization Header.
  *
  * @author themattharris
- * @version 0.8.4
+ * @version 0.8.4-dev
  *
- * 17 May 2014
+ * 18 May 2014
  */
 defined('__DIR__') or define('__DIR__', dirname(__FILE__));
 
 class tmhOAuth {
-  const VERSION = '0.8.4';
+  const VERSION = '0.8.4-dev';
   var $response = array();
 
   /**
